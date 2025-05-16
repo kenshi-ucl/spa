8360d424afd5de2555a2a809866050ab116d7c4b
kent <kent@gmail.com>
2025-05-16 12:23:10 +0800

    frontend

---
+++ script/app.js
@@ -0,0 +1,8 @@
+Alpine.store("settings", {
+    apiBaseUrl: "http://localhost:3000/kenshi/api",
+    appName: "exam application",
+    chatApiBaseUrl: "http://localhost:3000/kenshi/api/exam",  // merged chat info
+    chatAppName: "chat application",  // merged chat app name
+    shopApiBaseUrl: "http://localhost:3000/kenshi/api/shop",  // merged shop info
+    shopAppName: "shop application",  // merged shop app name
+});
